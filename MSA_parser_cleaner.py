b2480a26d95b8e3e8dca133f997425a4c5c7a4ef
yasinkaymaz <yasin.kaymaz@umassmed.edu>
2017-05-20 22:53:42 -0400

    code extention with repeats

--- MSA_parser_cleaner.py
+++ MSA_parser_cleaner.py
@@ -17,6 +17,17 @@ if sys.argv[3] == '1':
 else:
 	Ref='NC_009334'
 
+#Find the genomic locations fall into miropeats repeat regions
+RepeatList = []
+with open("/home/yasinkaymaz/EBV/NC_007605_miropeat_default_run_repeats.bed") as repeatFile:
+	for line in repeatFile:
+		repstart = int(line.strip().split("\t")[1])
+		repend = int(line.strip().split("\t")[2])
+		for i in range(repstart,repend):
+			RepeatList.append(i)
+print len(set(RepeatList))
+
+
 
 with open("AmplificationTest.aln", "r") as alnfile:
     df = pd.read_csv(alnfile,sep="\t",header=None)
@@ -46,7 +57,7 @@ with open("AmplificationTest.aln", "r") as alnfile:
 
     # for each row: 
     Ref_pos =0
-
+    MatchCount=0
     for i in range(len(str_seq1)):
         # place the the sequence in a position to a set
         set_seq = set(new_df.loc[:,i])
@@ -62,12 +73,12 @@ with open("AmplificationTest.aln", "r") as alnfile:
 
         if 'n' in set_pair or '-' in set_pair:
 		pass
-	elif len(set_pair)>1:
+	elif len(set_pair)>1 and Ref_pos not in RepeatList:
 		print set_pair, Ref_pos
-	
+	elif len(set_pair) == 1 and Ref_pos not in RepeatList:
+		MatchCount = MatchCount +1
 
-
-
+print "Match Count:", MatchCount
 
 
         # if the set has n or - pass it
